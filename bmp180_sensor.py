fecfd954b5ee999eefa0acd6b2e7dd05188b578b
dhkabayel <dhkabayel@gmail.com>
2021-01-10 12:41:57 +0300

    Update and rename bmp180_deneme.py to bmp180_sensor.py

--- bmp180_sensor.py
+++ bmp180_sensor.py
@@ -3,8 +3,8 @@ from bmp180 import BMP180
 import time
 from machine import I2C, Pin # create an I2C bus object accordingly to the port you are using77
 
-#bus = I2C(1, baudrate=100000)           # on pyboard
-i2cbmp =  I2C(scl=Pin(22), sda=Pin(1), freq=400000)   # on esp8266
+#bus = I2C(1, baudrate=100000)          
+i2cbmp =  I2C(scl=Pin(22), sda=Pin(1), freq=400000)   # esp8266
 
 bmp180 = BMP180(i2cbmp)
 bmp180.oversample_sett = 2
